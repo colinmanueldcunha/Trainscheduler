c5daea78d015a5eb3e81b682db9997fa3ab4048f
colinmanueldcunha <colinmdcunha@gmail.com>
2017-06-12 22:28:24 -0400

    completed

---
+++ assets/javascript/app.js
@@ -0,0 +1,103 @@
+/* global moment firebase */
+
+//
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyD07bRQT701MemcB5kEdcCJaXYChWhCDNk",
+    authDomain: "the-gmail-standard.firebaseapp.com",
+    databaseURL: "https://the-gmail-standard.firebaseio.com",
+    projectId: "the-gmail-standard",
+    storageBucket: "the-gmail-standard.appspot.com",
+    messagingSenderId: "193999930560"
+  };
+  firebase.initializeApp(config);
+
+//
+// Create a variable to reference the database.
+var database = firebase.database();
+
+// -----------------------------
+
+Button for adding trains
+$("#addTrainBtn").on("click", function(){
+
+// connectionsRef references a specific location in our database.
+// All of our connections will be stored in this directory.
+//var connectionsRef = database.ref("/connections");
+
+// '.info/connected' is a special location provided by Firebase that is updated
+// every time the client's connection state changes.
+// '.info/connected' is a boolean value, true if the client is connected and false if they are not.
+//var connectedRef = database.ref(".info/connected");
+
+  // Grabs user input
+  var trainName = $("#trainNameInput").val().trim();
+  var destination = $("#destinationInput").val().trim();
+  var firstTrainUnix = moment($("#firstTrainInput").val().trim(), "HH:mm").subtract(10, "years").format("X");
+  var frequency = $("#frequencyInput").val().trim();
+
+  // Creates local "temporary" object for holding train data
+  var newTrain = {
+    name:  trainName,
+    destination: destination,
+    firstTrain: firstTrainUnix,
+    frequency: frequency
+  }
+
+  // Uploads train data to the database
+  trainData.push(newTrain);
+
+  // Logs everything to console
+  console.log(newTrain.name);
+  console.log(newTrain.destination); 
+  console.log(firstTrainUnix);
+  console.log(newTrain.frequency)
+
+  // Alert
+  alert("Train successfully added");
+
+  // Clears all of the text-boxes
+  $("#trainNameInput").val("");
+  $("#destinationInput").val("");
+  $("#firstTrainInput").val("");
+  $("#frequencyInput").val("");
+
+  // Determine when the next train arrives.
+  return false;
+});
+
+
+// 4. Create Firebase event for adding trains to the database and a row in the html when a user adds an entry
+trainData.on("child_added", function(childSnapshot, prevChildKey){
+
+  console.log(childSnapshot.val());
+
+  // Store everything into a variable.
+  var tName = childSnapshot.val().name;
+  var tDestination = childSnapshot.val().destination;
+  var tFrequency = childSnapshot.val().frequency;
+  var tFirstTrain = childSnapshot.val().firstTrain;
+
+  // Calculate the minutes until arrival using hardcore math
+  // To calculate the minutes till arrival, take the current time in unix subtract the FirstTrain time and find the modulus between the difference and the frequency  
+  var differenceTimes = moment().diff(moment.unix(tFirstTrain), "minutes");
+  var tRemainder = moment().diff(moment.unix(tFirstTrain), "minutes") % tFrequency ;
+  var tMinutes = tFrequency - tRemainder;
+
+  // To calculate the arrival time, add the tMinutes to the currrent time
+  var tArrival = moment().add(tMinutes, "m").format("hh:mm A"); 
+  console.log(tMinutes);
+  console.log(tArrival);
+
+  console.log(moment().format("hh:mm A"));
+  console.log(tArrival);
+  console.log(moment().format("X"));
+
+  // Add each train's data into the table 
+  $("#trainTable > tbody").append("<tr><td>" + tName + "</td><td>" + tDestination + "</td><td>" + tFrequency + "</td><td>" + tArrival + "</td><td>" + tMinutes + "</td></tr>");
+
+});
+
+
+
+
